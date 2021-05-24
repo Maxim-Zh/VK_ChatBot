5c342f5a0e9cabe7740b228393a048c3985af74c
Maksim Zheleznov <zheleznov.maxim@gmail.com>
2021-05-24 18:11:33 +0300

    Bot now answer same message

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python 3
 import vk_api
-from vk_api import bot_longpoll
+import vk_api.bot_longpoll
+import random
 from my_token import token
 
 group_id = 204762908
@@ -11,7 +12,8 @@ class Bot:
         self.group_id = group_id
         self.token = token
         self.vk = vk_api.VkApi(token=token)
-        self.long_poller = bot_longpoll.VkBotLongPoll(self.vk, self.group_id)
+        self.long_poller = vk_api.bot_longpoll.VkBotLongPoll(vk=self.vk, group_id=self.group_id)
+        self.api = self.vk.get_api()
 
     def run(self):
         for event in self.long_poller.listen():
@@ -22,7 +24,16 @@ class Bot:
                 print(exc)
 
     def on_event(self, event):
-        print(event)
+        if event.type == vk_api.bot_longpoll.VkBotEventType.MESSAGE_NEW:
+            print(f'Получено сообщение "{event.object.message["text"]}" '
+                  f'от пользователя {event.object.message["from_id"]}')
+            # print(event)
+            self.api.messages.send(message=event.object.message["text"],
+                                   random_id=random.randint(0, 2 ** 20),
+                                   peer_id=event.object.message["peer_id"]
+                                   )
+        elif event.type == vk_api.bot_longpoll.VkBotEventType.MESSAGE_TYPING_STATE:
+            print('Пользователь печатает...')
 
 
 if __name__ == '__main__':
