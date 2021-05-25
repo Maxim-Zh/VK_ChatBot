1b860cb02be5679323ad8d1981134cd13166549c
Maksim Zheleznov <zheleznov.maxim@gmail.com>
2021-05-25 10:32:56 +0300

    Now logger is in place

--- bot.py
+++ bot.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python 3
 import vk_api
-import vk_api.bot_longpoll
 import random
+import logging
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from my_token import token
 
 group_id = 204762908
+log = logging.getLogger(name='bot_logger')
+log.setLevel(level=logging.DEBUG)
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(message)s'))
+log.addHandler(stream_handler)
+
 
 
 class Bot:
@@ -12,30 +19,28 @@ class Bot:
         self.group_id = group_id
         self.token = token
         self.vk = vk_api.VkApi(token=token)
-        self.long_poller = vk_api.bot_longpoll.VkBotLongPoll(vk=self.vk, group_id=self.group_id)
+        self.long_poller = VkBotLongPoll(vk=self.vk, group_id=self.group_id)
         self.api = self.vk.get_api()
 
     def run(self):
         for event in self.long_poller.listen():
-            print('Получено событие')
             try:
                 self.on_event(event=event)
-            except Exception as exc:
-                print(exc)
+            except Exception:
+                log.exception('Ошибка в обработке события')
 
     def on_event(self, event):
-        if event.type == vk_api.bot_longpoll.VkBotEventType.MESSAGE_NEW:
-            print(f'Получено сообщение "{event.object.message["text"]}" '
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            log.info(f'Получено сообщение "{event.object.message["text"]}" '
                   f'от пользователя {event.object.message["from_id"]}')
-            # print(event)
             self.api.messages.send(message='Сам ты ' + event.object.message["text"],
-                                   random_id=random.randint(0, 2 ** 20),
+                                   random_id=random.randint(0, 2 ** 25),
                                    peer_id=event.object.message["peer_id"]
                                    )
-        elif event.type == vk_api.bot_longpoll.VkBotEventType.MESSAGE_TYPING_STATE:
-            print('Пользователь печатает...')
+        elif event.type == VkBotEventType.MESSAGE_TYPING_STATE:
+            log.info(f'Пользователь {event.object["from_id"]} печатает...')
         else:
-            print(f'Я пока не умею это обрабатывать - {event.type}')
+            log.debug(f'Я пока не умею это обрабатывать - {event.type}')
 
 
 if __name__ == '__main__':
