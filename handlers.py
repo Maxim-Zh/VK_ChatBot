7aa191f5ade03a9992234ad3c6c1a0fde59ec0a7
Maksim Zheleznov <zheleznov.maxim@gmail.com>
2021-05-26 15:20:11 +0300

    Some handlers for Bot

---
+++ handlers.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python 3
+"""
+Handler - функция, принимающая на вход text (текст входящего сообщения) и context (dict, информация,
+собранная в ходе сценария), возвращает bool: True, если шаг пройден, False - если данные введены неправильно.
+"""
+import re
+
+re_name = re.compile(pattern=r'^[\w\-\s]{3,30}$')
+re_email = re.compile(pattern=r'\b[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+\b')
+
+
+def handle_name(text, context):
+    match = re.match(pattern=re_name, string=text)
+    if match:
+        context['name'] = text
+        return True
+    else:
+        return False
+
+
+def handle_email(text, context):
+    matches = re.findall(pattern=re_email, string=text)
+    if len(matches) > 0:
+        context['email'] = matches[0]
+        return True
+    else:
+        return False
